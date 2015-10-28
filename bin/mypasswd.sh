ebb33cf98e525530acd58ef5df2cc2eba11a1087
david-stratusee <david@stratusee.com>
2015-10-28 11:21:33 +0800

    Update bin/mypasswd.sh

--- bin/mypasswd.sh
+++ bin/mypasswd.sh
@@ -16,26 +16,71 @@ function upper_echo()
 	echo -e "$src" | awk '{print toupper($0)}'
 }
 
-if [ $# -lt 2 ]; then
-    echo "`basename $0` domain username"
+function pass_help()
+{
+    main_name=`basename $1`
+    echo "${main_name} domain username"
+    echo "${main_name} -e for enc_file"
+    echo "${main_name} -d for dec_file"
+}
+
+enc_key=david
+tmpfile=${HOME}/.mypasswd
+function dec_file()
+{
+    ccrypt -d -K ${enc_key} -q $1.cpt
+}
+
+function enc_file()
+{
+    ccrypt -e -K ${enc_key} -q $1
+}
+
+which ccrypt >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo We should install ccrypt to check password
     exit 1
 fi
 
-system=`uname -s`
+if [ $# -eq 1 ]; then
+    if [ "$1" == "-d" ]; then
+        dec_file ${tmpfile}
+        echo check ${tmpfile}
+        exit 0
+    elif [ "$1" == "-e" ]; then
+        enc_file ${tmpfile}
+        echo check ${tmpfile}.cpt
+        exit 0
+    fi
 
+    pass_help $0
+    exit 1
+fi
+
+if [ $# -lt 2 ]; then
+    pass_help $0
+    exit 1
+fi
 domain=$1
 username=$2
 domlen=${#domain}
-tmpfile=${HOME}/.mypasswd
 
-if [ -f ${tmpfile} ]; then
+if [ -f ${tmpfile}.cpt ]; then
+    dec_file ${tmpfile}
+    if [ $? -ne 0 ]; then
+        rm -f ${tmpfile}.cpt
+        exit 1
+    fi
+
     nitem=$(grep -c "${domain} ${username}" ${tmpfile})
     if [ $nitem -eq 1 ]; then
         echo $(grep "${domain} ${username}" ${tmpfile} | awk '{print $3}')
+        enc_file ${tmpfile}
         exit 0
     elif [ $nitem -gt 1 ]; then
         echo "More than one item is recorded in ${tmpfile}"
         grep "${domain} ${username}" ${tmpfile} | awk '{print $1" "$2}'
+        enc_file ${tmpfile}
         exit 1
     fi
 fi
@@ -55,3 +100,4 @@ ret2=${md5_ret:${ret2_start}:${ret2_len}}
 password="$ret1@`upper_echo $ret2`"
 echo $password
 echo "${domain} ${username} ${password}" >>${tmpfile}
+enc_file ${tmpfile}
